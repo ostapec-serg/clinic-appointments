d2a6e7af816db9f6cf8a341d4b99ae46e3b79a31
Sergii Ostapec <75143184+ostapec-serg@users.noreply.github.com>
2023-06-13 11:08:39 +0300

    Update filters.py

--- appointment/filters.py
+++ appointment/filters.py
@@ -1,1 +1,14 @@
-from django_filters import rest_framework as drf_filterfrom appointment.models import Appointmentclass AppointmentsFilter(drf_filter.FilterSet):    date = drf_filter.DateFilter(field_name='date')    class Meta:        model = Appointment        fields = [            'date'        ]
+from django_filters import rest_framework as drf_filter
+
+from appointment.models import Appointment
+
+
+class AppointmentsFilter(drf_filter.FilterSet):
+    date = drf_filter.DateFilter(field_name='date')
+
+    class Meta:
+        model = Appointment
+        fields = [
+            'date'
+        ]
+        
